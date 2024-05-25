73dea494191abf929a0f2b0de1d0460b1b84a89b
Nathan Aschenbach <thedenrei@gmail.com>
2024-05-25 16:34:53 -0700

    l4cd is better than apds

---
+++ lib/Gesture/gesture.h
@@ -0,0 +1,2 @@
+void _gesture_setup();
+void _gesture_loop();
--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,34 @@
 #include <Arduino.h>
+
+#define PROXIMITY
+#ifdef PROXIMITY
 #include "l4cd.h"
 #include "apds.h"
+#else
+#include "gesture.h"
+#endif
 
 void setup() {
   Serial.begin(9600);
-
+#ifdef PROXIMITY
   _setup_l4cd();
   _setup_apds();
+#else
+  _gesture_setup();
+#endif
 }
 
 void loop() {
   // Serial.print("Time: ");
   // Serial.println(millis());
-
+#ifdef PROXIMITY
   _loop_l4cd();
   _loop_apds();
-
   Serial.print("Proximity:\t");
   Serial.print(_l4cd_get_proximity());
   Serial.print(", \t");
   Serial.println(_apds_get_proximity());
+#else
+  _gesture_loop();
+#endif
 }
